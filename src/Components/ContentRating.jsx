b8a00e6a7b258548b3dee7712616c0d5399319c1
Sandip Maharaj <maharaj_sandip@berkeley.edu>
2025-10-21 00:47:50 -0400

    complete the like, dislike and total likes

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,45 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0,
+        dislikes: 0,
+        totalRatings: 0,
+
+        handlelike: () => {
+            this.setState((prevState, totalRatings) => ({
+                likes: prevState.likes + 1,
+                totalRatings: prevState.totalRatings + 1
+            }));
+        },
+
+        handledislike: () => {
+            this.setState((prevState, totalRatings) => ({
+                dislikes: prevState.dislikes + 1,
+                totalRatings: prevState.totalRatings + 1
+            }));
+        }
+    }
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className='content-rating'>
+        <p>
+            // text goes here
+        </p>
+
+        <div className='rating-buttons'>
+            <button className='like-button' onClick={this.state.handlelike}>
+                Like ({this.state.likes})
+            </button>
+
+            <button className='dislike-button' onClick={this.state.handledislike}>
+                Dislike ({this.state.dislikes})
+            </button>
+        </div>
+        <p>Total Ratings {this.state.totalRatings}</p>
+     </div>
      </>
     );
   }
